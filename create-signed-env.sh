ce533548dd2ec06b2b9eef8222d7763dd5872c6f
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-02-15 14:10:12 +0200

    Update create-signed-env.sh

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
 
 # Default values
-DEFAULT_COUNTRY="US"
-DEFAULT_STATE="California"
-DEFAULT_LOCALITY="Mountain View"
-DEFAULT_ORGANIZATION="crDroid"
-DEFAULT_ORG_UNIT="crDroid"
-DEFAULT_COMMON_NAME="crDroid"
-DEFAULT_EMAIL="contact@crdroid.net"
-
-# Prompt the user for each part of the subject line with defaults
-read -p "Enter country code [${DEFAULT_COUNTRY}] (C): " country
-country=${country:-$DEFAULT_COUNTRY}
-
-read -p "Enter state or province name [${DEFAULT_STATE}] (ST): " state
-state=${state:-$DEFAULT_STATE}
-
-read -p "Enter locality [${DEFAULT_LOCALITY}] (L): " locality
-locality=${locality:-$DEFAULT_LOCALITY}
-
-read -p "Enter organization name [${DEFAULT_ORGANIZATION}] (O): " organization
-organization=${organization:-$DEFAULT_ORGANIZATION}
-
-read -p "Enter organizational unit [${DEFAULT_ORG_UNIT}] (OU): " organizational_unit
-organizational_unit=${organizational_unit:-$DEFAULT_ORG_UNIT}
-
-read -p "Enter common name [${DEFAULT_COMMON_NAME}] (CN): " common_name
-common_name=${common_name:-$DEFAULT_COMMON_NAME}
-
-read -p "Enter email address [${DEFAULT_EMAIL}] (emailAddress): " email
-email=${email:-$DEFAULT_EMAIL}
+country="DE"
+state="Bavaria"
+locality="Munich"
+organization="Mi439"
+organizational_unit="Mi439"
+common_name="Mi439"
+email="Mi439@Mi439.com"
 
 # Construct the subject line
 subject="/C=${country}/ST=${state}/L=${locality}/O=${organization}/OU=${organizational_unit}/CN=${common_name}/emailAddress=${email}"
@@ -40,16 +18,8 @@ echo "Using Subject Line:"
 echo "$subject"
 echo ""
 
-# Prompt the user to verify if the subject line is correct
-read -p "Is the subject line correct? [Y/n]: " confirmation
-confirmation=${confirmation:-Y}
-
-# Check the user's response
-if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
-    echo "Exiting without changes."
-    exit 1
-fi
-clear
+# https://wiki.lineageos.org/signing_builds
+rm -rf ~/.android-certs
 
 make_key_nopass() {
   local out="$1"
